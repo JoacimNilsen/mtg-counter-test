0bfba96d4084003866d7f72d430ae6bc034b9984
JoacimNilsen <joacimnilsen1986@gmail.com>
2020-09-09 00:45:31 +0200

    Added resetbutton

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,6 +5,11 @@ const Counter = () => {
   const [player1, setPlayer1] = useState(20)
   const [player2, setPlayer2] = useState(20)
 
+  const clearState = () => {
+    setPlayer1(20)
+    setPlayer2(20)
+  }
+
   return (
     <div>
       <p>Player 1:{player1}</p>
@@ -13,6 +18,7 @@ const Counter = () => {
       <p>Player 2:{player2}</p>
       <button onClick={() => setPlayer2(player2 + 1)}>+</button>
       <button onClick={() => setPlayer2(player2 - 1)}>-</button>
+      <button onClick={() => clearState()}>O</button>
     </div>
   )
 }
