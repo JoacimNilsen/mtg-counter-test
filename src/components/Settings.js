ce5c9e4bbcdcf9267cf78d5da689e75ba136a333
JoacimNilsen <joacimnilsen1986@gmail.com>
2021-02-21 17:43:51 +0100

    made black, more black

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -10,7 +10,7 @@ const Settings = ({handleP1Change, handleP2Change}) => {
 <select onChange={handleP1Change} name="cars" id="cars">
     <option value="194, 33, 33, 1">Red</option>
     <option value="53, 191, 25, 1">Green</option>
-    <option value="52, 50, 50, 1">Black</option>
+    <option value="10, 10, 10, 1">Black</option>
     <option value="242, 242, 242, 1">White</option>
     <option value="0, 51, 153, 1">Blue</option>
    
@@ -18,7 +18,7 @@ const Settings = ({handleP1Change, handleP2Change}) => {
 <select onChange={handleP2Change} name="cars" id="cars">
     <option value="194, 33, 33, 1">Red</option>
     <option value="53, 191, 25, 1">Green</option>
-    <option value="52, 50, 50, 1">Black</option>
+    <option value="10, 10, 10, 1">Black</option>
     <option value="242, 242, 242, 1">White</option>
     <option value="0, 51, 153, 1">Blue</option>
   
