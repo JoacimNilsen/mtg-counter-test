7d3e598f44cbecf7a6bf262406c590ffd6ec3afd
JoacimNilsen <joacimnilsen1986@gmail.com>
2021-03-08 11:18:17 +0100

    Styled and positioned centered buttons

--- src/components/Buttons/Settings.js
+++ src/components/Buttons/Settings.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 import styled from 'styled-components'
 
 const StyledSettings = styled.div`
-width: 50px;
+width: 5em;
 border: none;
 background-color:transparent;
 outline:none;
--- src/components/Buttons/Wakelock.js
+++ src/components/Buttons/Wakelock.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import ReactNoSleep from 'react-no-sleep'
+import styled from 'styled-components'
+
+const NoSleep = styled.button`
+ display: inline-block;
+ width: 100px;
+ border: 0.4em solid #000000;
+ border-radius: 1em;
+ box-sizing: border-box;
+ text-decoration: none;
+ font-family: 'Roboto',sans-serif;
+ font-weight: 300;
+ color: #000000;
+ text-shadow: 0 0.04em 0.04em rgba(0,0,0,0.35);
+ background-color: transparent;
+ text-align: center;
+ outline: none;
+`
 
 const Wakelock = () => {
   return(
 
 <ReactNoSleep>
+  
 {({ isOn, enable, disable }) => (
-  <button onClick={isOn ? disable : enable}>
-    {isOn ? 'on' : 'off'}
-  </button>
+  <NoSleep onClick={isOn ? disable : enable}>
+    {isOn ? 'Wakelock: ON ' : 'Wakelock: OFF'}
+  </NoSleep>
 )}
 </ReactNoSleep>
 )
